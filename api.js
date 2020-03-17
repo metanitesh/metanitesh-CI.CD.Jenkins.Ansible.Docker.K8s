3b2e98e34930b723132a5f60412083be0905be6a
Nitesh Sharma <nitesh.grep@gmail.com>
2020-03-17 14:39:24 +0530

    Adding simple API

---
+++ api.js
@@ -0,0 +1,22 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World! <br><br> <a href="/courses">courses</a>'))
+app.get('/courses', (req, res) => res.json([{
+  id:1,
+  name:'cloud-devops'
+},{
+  id:2,
+  name:'cloud-developer'
+},{
+  id:3,
+  name:'data-engineer'
+},{
+  id:1,
+  name:'machine-learning'
+}]))
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
