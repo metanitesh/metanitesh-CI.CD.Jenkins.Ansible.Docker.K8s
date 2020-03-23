04e3fd7e0185efdb7d41d0ab662bc2811cd2402c
Nitesh Sharma <nitesh.grep@gmail.com>
2020-03-23 08:06:13 +0530

    failing linter

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000
 
 app.get('/', (req, res) => res.send('Green deployment! <br><br> <a href="/courses">courses</a>'));
 app.get('/courses', (req, res) => res.json([{
